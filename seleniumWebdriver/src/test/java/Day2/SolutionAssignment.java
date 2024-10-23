9d90233b944f5cdd4fef40928187dcca2ce8f678
Subhadip Das <subhadipdas911@gmail.com>
2024-10-23 20:07:04 +0530

    Solved the Assignment for Day2

---
+++ seleniumWebdriver/src/test/java/Day2/SolutionAssignment.java
@@ -0,0 +1,30 @@
+package Day2;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SolutionAssignment {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.demoblaze.com/index.html");
+		
+		List<WebElement> totalLinks = driver.findElements(By.tagName("a"));
+		
+		System.out.println("Total Links : "+totalLinks.size());
+		
+		List<WebElement> totalImages = driver.findElements(By.tagName("img"));
+		
+		System.out.println("Total Images : "+totalImages.size());
+		
+		driver.findElement(By.linkText("Monitors")).click();
+
+	}
+
+}
