28d5eaaab46a3aa41d0a428322f59ede69a71fff
Subhadip Das <subhadipdas911@gmail.com>
2024-10-24 11:35:42 +0530

    Using relative xpath to click the MacBook

--- seleniumWebdriver/src/test/java/Day4/XPathLocator.java
+++ seleniumWebdriver/src/test/java/Day4/XPathLocator.java
@@ -12,7 +12,11 @@ public class XPathLocator {
 		
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[2]/div[1]/input[1]")).sendKeys("Tshirt");
+//		driver.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[2]/div[1]/input[1]")).sendKeys("Tshirt"); // absolute xPath
+		
+//		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Laptop"); // relative xpath
+		
+		driver.findElement(By.xpath("//img[@title=\"MacBook\"]")).click(); // using relative xpath to click the MacBook
 		
 		
 	}
