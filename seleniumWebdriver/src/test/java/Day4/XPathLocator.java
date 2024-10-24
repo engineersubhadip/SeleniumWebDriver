72ac7d8bfb04f061a77faf2dac07633474af0b63
Subhadip Das <subhadipdas911@gmail.com>
2024-10-24 15:12:32 +0530

    xPath

--- seleniumWebdriver/src/test/java/Day4/XPathLocator.java
+++ seleniumWebdriver/src/test/java/Day4/XPathLocator.java
@@ -1,8 +1,13 @@
 package Day4;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class XPathLocator {
 
@@ -15,32 +20,40 @@ public class XPathLocator {
 		
 //		driver.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[1]/div[2]/div[1]/input[1]")).sendKeys("Tshirt"); // absolute xPath
 		
-//		xpath with single attribute :- 
+//		xpath with single attribute :-  (Working)
 		
 		
 //		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Laptop"); // relative xpath
 		
 //		driver.findElement(By.xpath("//img[@title=\"MacBook\"]")).click(); // using relative xpath to click the MacBook
 		
-//		xpath with multiple attributes :-
+//		xpath with multiple attributes :-(working)
 		
 //		driver.findElement(By.xpath("//input[@name='search'][@placeholder='Search']")).sendKeys("Toffee");
 		
 		
-//		capturing the inner text
+//		capturing the inner text (working)
 		
 //		String text = driver.findElement(By.xpath("//h3[text()='Featured']")).getText();
 //		System.out.println(text);
 		
 		
-//		driver.findElement(By.xpath("//a[text()='MacBook']")).click();
 		
-//		xpath with innertext
+//		xpath with innertext (working)
 		
 //		driver.findElement(By.linkText("Tablets")).click();
 		
 //		driver.findElement(By.xpath("//a[text()='Tablets']")).click();
 		
+		
+//		driver.findElement(By.xpath("//a[text()='MacBook']")).click(); // getting ElementClickInterceptedException
+		
+//		WebElement res = driver.findElement(By.linkText("MacBook"));// getting ElementClickInterceptedException
+//		res.click();//// getting ElementClickInterceptedException
+		
+//		xpath with contains :- (Working)
+		
+		driver.findElement(By.xpath("//input[contains(@placeholder,'Sear')]")).sendKeys("toffee");
 		
 	}
 
